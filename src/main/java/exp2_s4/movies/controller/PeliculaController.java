e79ad192832c4d7c8c19c97e759e02dd217ec09f
Hugo Ramos <hu.ramos@duocuc.cl>
2024-09-09 14:19:30 -0300

    Se agrega CRUD

--- src/main/java/exp2_s4/movies/controller/PeliculaController.java
+++ src/main/java/exp2_s4/movies/controller/PeliculaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import exp2_s4.movies.model.Pelicula;
 import exp2_s4.movies.service.PeliculaService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/peliculas")
@@ -26,5 +30,20 @@ public class PeliculaController {
     @GetMapping("/{id}")
     public Optional<Pelicula> getPeliculaById(@PathVariable Long id) {
         return peliculaService.getPeliculaById(id);
+    }
+
+    @PostMapping
+    public Pelicula createPelicula(@RequestBody Pelicula pelicula) {
+        return peliculaService.createPelicula(pelicula);
+    }
+
+    @PutMapping("/{id}")
+    public Pelicula updatePelicula(@PathVariable Long id, @RequestBody Pelicula pelicula) {
+        return peliculaService.updatePelicula(id, pelicula);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePelicula(@PathVariable Long id) {
+        peliculaService.deletePelicula(id);
     }
 }
